a51bf7fc377abaa00ed32e173a8a51e7e6224859
iimm <liudichen@foxmail.com>
2022-12-21 17:11:08 +0800

    ->1.0.3

--- src/interface.tsx
+++ src/interface.tsx
@@ -1,13 +1,17 @@
-import { OptionsObject, SnackbarMessage, SnackbarKey, SnackbarProviderProps } from 'notistack';
+import type { ReactNode } from 'react';
+import type { OptionsObject, SnackbarMessage, SnackbarKey, SnackbarProviderProps } from 'notistack';
 
 export interface MessageContext {
   enqueueSnackbar: (message: SnackbarMessage, options?: OptionsObject) => SnackbarKey;
   closeSnackbar: (key?: SnackbarKey) => void;
 }
 
-export interface MessageBoxProps extends SnackbarProviderProps {
+export interface MessageBoxProps extends Omit<SnackbarProviderProps, 'children'> {
   /** responsive for dense?根据宽度响应式设置dense属性?与breakpoint配合使用 */
   responsive?: boolean,
-  /** breakpoint of responsive for dense? */
+  /** breakpoint of responsive for dense? 
+   * @default 'md'
+  */
   breakpoint?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | number,
+  children?: ReactNode | ReactNode[],
 }
