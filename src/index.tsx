5f5f2a4d28be1a7dc4ee8a728ad790542ad542e7
iimm <liudichen@foxmail.com>
2024-05-06 09:30:12 +0800

    1.0.4 移除defaultProps

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,29 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import React from 'react';
-import { SnackbarProvider, OptionsObject, SnackbarKey, SnackbarMessage } from 'notistack';
-import { IconButton, useTheme, useMediaQuery } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
+import React from "react";
+import { SnackbarProvider, OptionsObject, SnackbarKey, SnackbarMessage } from "notistack";
+import { IconButton, useTheme, useMediaQuery } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
-import { MessageContext, MessageBoxProps } from './interface';
-import useMessage from './useMessage';
-import MessageProvider from './MessageProvider';
+import { MessageContext, MessageBoxProps } from "./interface";
+import useMessage from "./useMessage";
+import MessageProvider from "./MessageProvider";
 
 const messageRef = React.createRef<MessageContext>();
 
-const message = (message: SnackbarMessage, option ?:OptionsObject) => messageRef.current?.enqueueSnackbar(message, { variant: 'default', ...(option || {}) });
+const message = (message: SnackbarMessage, option?: OptionsObject) =>
+  messageRef.current?.enqueueSnackbar(message, { variant: "default", ...(option || {}) });
 
-const info = (message: SnackbarMessage, option ?:OptionsObject) => messageRef.current?.enqueueSnackbar(message, { ...(option || {}), variant: 'info' });
+const info = (message: SnackbarMessage, option?: OptionsObject) =>
+  messageRef.current?.enqueueSnackbar(message, { ...(option || {}), variant: "info" });
 
-const warning = (message: SnackbarMessage, option ?:OptionsObject) => messageRef.current?.enqueueSnackbar(message, { ...(option || {}), variant: 'warning' });
+const warning = (message: SnackbarMessage, option?: OptionsObject) =>
+  messageRef.current?.enqueueSnackbar(message, { ...(option || {}), variant: "warning" });
 
-const success = (message: SnackbarMessage, option ?:OptionsObject) => messageRef.current?.enqueueSnackbar(message, { ...(option || {}), variant: 'success' });
+const success = (message: SnackbarMessage, option?: OptionsObject) =>
+  messageRef.current?.enqueueSnackbar(message, { ...(option || {}), variant: "success" });
 
-const error = (message: SnackbarMessage, option ?:OptionsObject) => messageRef.current?.enqueueSnackbar(message, { ...(option || {}), variant: 'error' });
+const error = (message: SnackbarMessage, option?: OptionsObject) =>
+  messageRef.current?.enqueueSnackbar(message, { ...(option || {}), variant: "error" });
 
 const destroy = () => messageRef.current?.closeSnackbar();
 
@@ -28,42 +33,44 @@ message.warning = warning;
 message.error = error;
 message.destroy = destroy;
 
-const MessageBox: React.FC<MessageBoxProps> = (props: MessageBoxProps) => {
-  const { children, dense, responsive = true, breakpoint = 'md', ...restProps } = props;
-  const theme = useTheme();
-  const down = useMediaQuery(theme.breakpoints.down(breakpoint));
-  return (
-    // @ts-ignore
-    <SnackbarProvider dense={dense ?? (responsive ? down : undefined)} {...restProps} ref={messageRef}>
-      <MessageProvider message={message}>
-        {children}
-      </MessageProvider>
-    </SnackbarProvider>
-  );
-};
-
-MessageBox.defaultProps = {
+const defaultProps = {
   maxSnack: 3,
   autoHideDuration: 2000,
   anchorOrigin: {
-    vertical: 'top',
-    horizontal: 'center',
+    vertical: "top",
+    horizontal: "center",
   },
   action: (key?: SnackbarKey) => {
     return (
-      <IconButton key='close' aria-label='Close' color='inherit' onClick={() => {
-        messageRef.current?.closeSnackbar(key);
-      }}>
-        <CloseIcon style={{ fontSize: '20px' }} />
+      <IconButton
+        key="close"
+        aria-label="Close"
+        color="inherit"
+        onClick={() => {
+          messageRef.current?.closeSnackbar(key);
+        }}
+      >
+        <CloseIcon style={{ fontSize: "20px" }} />
       </IconButton>
     );
   },
 };
 
-export {
-  messageRef,
-  message,
-  MessageBox,
-  MessageBoxProps,
-  useMessage,
+const MessageBox: React.FC<MessageBoxProps> = (props: MessageBoxProps) => {
+  const { children, dense, responsive = true, breakpoint = "md", ...restProps } = props;
+  const theme = useTheme();
+  const down = useMediaQuery(theme.breakpoints.down(breakpoint));
+  return (
+    // @ts-ignore
+    <SnackbarProvider
+      dense={dense ?? (responsive ? down : undefined)}
+      {...defaultProps}
+      {...restProps}
+      ref={messageRef as any}
+    >
+      <MessageProvider message={message}>{children}</MessageProvider>
+    </SnackbarProvider>
+  );
 };
+
+export { messageRef, message, MessageBox, MessageBoxProps, useMessage };
